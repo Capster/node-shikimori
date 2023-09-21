2ffa74539c87eff14aeae459d7223587436cfe7f
Maxim <Capster@users.noreply.github.com>
2023-09-22 00:50:21 +0300

    Move common types to a separate file

---
+++ src/types/common.ts
@@ -0,0 +1,21 @@
+export type Notice = Record<'notice', string>;
+export type NoticeSuccess = Notice & Record<'success', boolean>;
+
+export type DateTime = string;
+
+export interface Linkable {}
+export type LinkedId = number;
+export type LinkedType = 'Anime'
+  | 'Manga'
+  | 'Ranobe'
+  | 'Character'
+  | 'Person'
+  | 'Club'
+  | 'ClubPage'
+  | 'Critique'
+  | 'Review'
+  | 'Contest'
+  | 'CosplayGallery'
+  | 'Collection'
+  | 'Article'
+  | null;
