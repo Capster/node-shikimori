2f3f2c0876c4aeee20054cb47250fab4bdc55a23
Maxim <Capster@users.noreply.github.com>
2023-10-06 23:11:20 +0300

    Add documentation to the constant variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
+/** The base URL for the Shikimori API */
 export const BASE_URL = 'https://shikimori.me';
+/** The path to the OAuth2 token endpoint */
 export const TOKEN_PATH = '/oauth/token';
+/** The default user agent string used when making API requests */
 export const DEFAULT_USER_AGENT = 'node-shikimori';
+/** The maximum number of API calls that can be made per second */
 export const MAX_CALLS_PER_SECOND = 5;
+/** The maximum number of API calls that can be made per minute */
 export const MAX_CALLS_PER_MINUTE = 90;
