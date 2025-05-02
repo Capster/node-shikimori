4e9f0e80fb516e0c4ddd76f001589d2215cd16c3
Maxim <Capster@users.noreply.github.com>
2025-05-03 01:49:02 +0300

    Fix an issue that ranobe.* endpoints were sending requests to animes/*

--- src/endpoints/ranobe.ts
+++ src/endpoints/ranobe.ts
@@ -16,27 +16,27 @@ import {
 import { Id } from "./common";
 
 export interface RanobeParams {
-  page?: number,
-  limit?: number,
-  order?: RanobeOrder,
-  kind?: RanobeKind,
-  status?: RanobeStatus,
-  season?: string,
-  score?: number,
-  genre?: string,
-  publisher?: string,
-  franchise?: string,
-  censored?: boolean,
-  mylist?: UserRateStatus,
-  ids?: string,
-  exclude_ids?: string,
-  search?: string,
+  page?: number;
+  limit?: number;
+  order?: RanobeOrder;
+  kind?: RanobeKind;
+  status?: RanobeStatus;
+  season?: string;
+  score?: number;
+  genre?: string;
+  publisher?: string;
+  franchise?: string;
+  censored?: boolean;
+  mylist?: UserRateStatus;
+  ids?: string;
+  exclude_ids?: string;
+  search?: string;
 }
 
 export interface RanobeTopicsParams {
-  id: RanobeId,
-  page?: number,
-  limit?: number,
+  id: RanobeId;
+  page?: number;
+  limit?: number;
 }
 
 /**
@@ -48,65 +48,60 @@ export const ranobe = ({ get }: RequestMethods) => {
    * List ranobe
    * @param params
    */
-  const list = (params: RanobeParams): Promise<RanobeBasic[]> => (
-    get(`/animes`, params)
-  );
+  const list = (params: RanobeParams): Promise<RanobeBasic[]> =>
+    get(`/ranobes`, params);
 
   /**
    * Get a ranobe by `RanobeId`
    * @param params
    */
-  const byId = ({ id }: Id<RanobeId>): Promise<Ranobe> => (
-    get(`/animes/${id}`, {})
-  );
+  const byId = ({ id }: Id<RanobeId>): Promise<Ranobe> =>
+    get(`/ranobes/${id}`, {});
 
   /**
    * List ranobe roles
    * @param params
    */
-  const roles = ({ id }: Id<RanobeId>): Promise<Role[]> => (
-    get(`/animes/${id}/roles`, {})
-  );
+  const roles = ({ id }: Id<RanobeId>): Promise<Role[]> =>
+    get(`/ranobes/${id}/roles`, {});
 
   /**
    * List similar ranobe
    * @param params
    */
-  const similar = ({ id }: Id<RanobeId>): Promise<RanobeBasic[]> => (
-    get(`/animes/${id}/similar`, {})
-  );
+  const similar = ({ id }: Id<RanobeId>): Promise<RanobeBasic[]> =>
+    get(`/ranobes/${id}/similar`, {});
 
   /**
    * List related ranobe
    * @param params
    */
-  const related = ({ id }: Id<RanobeId>): Promise<RanobeRelation[]> => (
-    get(`/animes/${id}/relation`, {})
-  );
+  const related = ({ id }: Id<RanobeId>): Promise<RanobeRelation[]> =>
+    get(`/ranobes/${id}/relation`, {});
 
   /**
    * List the whole franchise
    * @param params
    */
-  const franchise = ({ id }: Id<RanobeId>): Promise<Franchise> => (
-    get(`/animes/${id}/franchise`, {})
-  );
+  const franchise = ({ id }: Id<RanobeId>): Promise<Franchise> =>
+    get(`/ranobes/${id}/franchise`, {});
 
   /**
    * List external links of a ranobe
    * @param params
    */
-  const externalLinks = ({ id }: Id<RanobeId>): Promise<ExternalLink[]> => (
-    get(`/animes/${id}/external_links`, {})
-  );
+  const externalLinks = ({ id }: Id<RanobeId>): Promise<ExternalLink[]> =>
+    get(`/ranobes/${id}/external_links`, {});
 
   /**
    * List ranobe topics
    * @param params
    */
-  const topics = ({ id, ...params }: RanobeTopicsParams): Promise<Topic<RanobeBasic>[]> => (
-    get(`/animes/${id}/topics`, params)
-  );
+  const topics = ({
+    id,
+    ...params
+  }: RanobeTopicsParams): Promise<Topic<RanobeBasic>[]> =>
+    get(`/ranobes/${id}/topics`, params);
 
   return {
     list,
