47e695385618a3791e16ceede8a3366e9c361ff9
Maxim <Capster@users.noreply.github.com>
2025-05-03 01:46:37 +0300

    Fix an issue that mangas.* endpoints were sending requests to animes/*

--- src/endpoints/mangas.ts
+++ src/endpoints/mangas.ts
@@ -11,32 +11,32 @@ import {
   MangaStatus,
   ExternalLink,
   MangaRelation,
-  UserRateStatus
+  UserRateStatus,
 } from "../types";
 import { Id } from "./common";
 
 export interface MangasParams {
-  page?: number,
-  limit?: number,
-  order?: MangaOrder,
-  kind?: MangaKind,
-  status?: MangaStatus,
-  season?: string,
-  score?: number,
-  genre?: string,
-  publisher?: string,
-  franchise?: string,
-  censored?: boolean,
-  mylist?: UserRateStatus,
-  ids?: string,
-  exclude_ids?: string,
-  search?: string,
+  page?: number;
+  limit?: number;
+  order?: MangaOrder;
+  kind?: MangaKind;
+  status?: MangaStatus;
+  season?: string;
+  score?: number;
+  genre?: string;
+  publisher?: string;
+  franchise?: string;
+  censored?: boolean;
+  mylist?: UserRateStatus;
+  ids?: string;
+  exclude_ids?: string;
+  search?: string;
 }
 
 export interface MangaTopicsParams {
-  id: MangaId,
-  page?: number,
-  limit?: number,
+  id: MangaId;
+  page?: number;
+  limit?: number;
 }
 
 /**
@@ -48,65 +48,60 @@ export const mangas = ({ get }: RequestMethods) => {
    * List mangas
    * @param params
    */
-  const list = (params: MangasParams): Promise<MangaBasic[]> => (
-    get(`/animes`, params)
-  );
+  const list = (params: MangasParams): Promise<MangaBasic[]> =>
+    get(`/mangas`, params);
 
   /**
    * Get a manga by `MangaId`
    * @param params
    */
-  const byId = ({ id }: Id<MangaId>): Promise<Manga> => (
-    get(`/animes/${id}`, {})
-  );
+  const byId = ({ id }: Id<MangaId>): Promise<Manga> =>
+    get(`/mangas/${id}`, {});
 
   /**
    * List manga roles
    * @param params
    */
-  const roles = ({ id }: Id<MangaId>): Promise<Role[]> => (
-    get(`/animes/${id}/roles`, {})
-  );
+  const roles = ({ id }: Id<MangaId>): Promise<Role[]> =>
+    get(`/mangas/${id}/roles`, {});
 
   /**
    * List similar manga
    * @param params
    */
-  const similar = ({ id }: Id<MangaId>): Promise<MangaBasic[]> => (
-    get(`/animes/${id}/similar`, {})
-  );
+  const similar = ({ id }: Id<MangaId>): Promise<MangaBasic[]> =>
+    get(`/mangas/${id}/similar`, {});
 
   /**
    * List related manga
    * @param params
    */
-  const related = ({ id }: Id<MangaId>): Promise<MangaRelation[]> => (
-    get(`/animes/${id}/relation`, {})
-  );
+  const related = ({ id }: Id<MangaId>): Promise<MangaRelation[]> =>
+    get(`/mangas/${id}/relation`, {});
 
   /**
    * List the whole franchise
    * @param params
    */
-  const franchise = ({ id }: Id<MangaId>): Promise<Franchise> => (
-    get(`/animes/${id}/franchise`, {})
-  );
+  const franchise = ({ id }: Id<MangaId>): Promise<Franchise> =>
+    get(`/mangas/${id}/franchise`, {});
 
   /**
    * List external links of a manga
    * @param params
    */
-  const externalLinks = ({ id }: Id<MangaId>): Promise<ExternalLink[]> => (
-    get(`/animes/${id}/external_links`, {})
-  );
+  const externalLinks = ({ id }: Id<MangaId>): Promise<ExternalLink[]> =>
+    get(`/mangas/${id}/external_links`, {});
 
   /**
    * List manga topics
    * @param params
    */
-  const topics = ({ id, ...params }: MangaTopicsParams): Promise<Topic<MangaBasic>[]> => (
-    get(`/animes/${id}/topics`, params)
-  );
+  const topics = ({
+    id,
+    ...params
+  }: MangaTopicsParams): Promise<Topic<MangaBasic>[]> =>
+    get(`/mangas/${id}/topics`, params);
 
   return {
     list,
